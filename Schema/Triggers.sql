54d35a5b4730744d78adf9abec37d348c09c5bfa
Jfoose89 <jordan.foose@chasacademy.se>
2025-12-02 14:15:30 +0100

    Create unique constraints for multiple tables
    Added unique constraints to Role, Resources, and Report tables.

---
+++ Schema/Triggers.sql
@@ -0,0 +1,13 @@
+// Unique Constraints query
+
+USE DatanautDB;
+GO
+
+ALTER TABLE Role
+ADD CONSTRAINT UQ_Role_RoleName UNIQUE (RoleName);
+
+ALTER TABLE Resources
+ADD CONSTRAINT UQ_Resources_Name_Project UNIQUE (ResourceName, FKProjectID);
+
+ALTER TABLE Report
+ADD CONSTRAINT UQ_Report_Project_DateRange UNIQUE (FKProjectID, ReportStartDate, ReportEndDate);
