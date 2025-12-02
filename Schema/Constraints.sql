5ac5538f434a65c59690376976166638f2ad12fd
Jfoose89 <jordan.foose@chasacademy.se>
2025-12-02 15:00:42 +0100

    Add FK constraint for ProjectManager in PROJECT table
    Added foreign key constraint for ProjectManager in PROJECT table.

--- Schema/Constraints.sql
+++ Schema/Constraints.sql
@@ -11,3 +11,8 @@ ADD CONSTRAINT UQ_Resources_Name_Project UNIQUE (ResourceName, FKProjectID);
 
 ALTER TABLE Report
 ADD CONSTRAINT UQ_Report_Project_DateRange UNIQUE (FKProjectID, ReportStartDate, ReportEndDate);
+
+ALTER TABLE PROJECT
+ADD FKProjectManagerID INT NOT NULL,
+	CONSTRAINT FK_Project_ProjectManager
+	FOREIGN KEY (FKProjectManagerID) REFERENCES ProjectManager(PKProjectMangerID);
